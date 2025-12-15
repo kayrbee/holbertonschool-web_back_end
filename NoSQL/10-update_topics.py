e1247c560206ee0499533395538b2f2366768610
Katherine Beringer <github@katbee.au>
2025-12-15 12:51:09 +1100

    Set multi to true

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -7,5 +7,5 @@ def update_topics(mongo_collection, name, topics):
     """ Now it's documented"""
     search = {"name": name}
     new_values = {"$set": {"topics": topics}}
-    school_id = mongo_collection.update(search, new_values)
+    school_id = mongo_collection.update(search, new_values, {"multi": True})
     return school_id
