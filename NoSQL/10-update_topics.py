14811789b9e9341a84a6064f7ebe3b35709efbfc
Katherine Beringer <github@katbee.au>
2025-12-15 12:47:43 +1100

    try deprecated method first

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Write a Python function that changes all topics of a
+ school document based on the name"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """ Now it's documented"""
+    search = {"name": name}
+    new_values = {"$set": {"topics": topics}}
+    school_id = mongo_collection.update(search, new_values)
+    return school_id
