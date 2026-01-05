a6f046cce807dc2ccec4d7e55f677067a4889953
Katherine Beringer <github@katbee.au>
2026-01-05 16:11:20 +1100

    Fixed bug in array assignment

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,8 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-let newArray = [];
-  for (let idx of array) {
-    newArray[idx] = appendString + idx;
+    let index = 0;
+    for (const idx of array) {
+    array[index] = appendString + idx;
+    index += 1;
   }
 
-  return newArray;
+  return array;
 }
