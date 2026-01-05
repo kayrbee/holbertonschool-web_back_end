c4508697107384eeef04a791458c65ef8712aaa1
Katherine Beringer <github@katbee.au>
2026-01-05 15:45:04 +1100

    Refactored to use for...of loops

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (var idx in array) {
-    var value = array[idx];
-    array[idx] = appendString + value;
+  for (let idx of array) {
+    array[idx] = appendString + idx;
   }
 
   return array;
