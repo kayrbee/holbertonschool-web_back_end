891f07dde28a0730f3fc68761310289d860d963d
Katherine Beringer <github@katbee.au>
2026-01-06 13:46:58 +1100

    Refactored to use inline objects

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,26 +1,7 @@
 export default function getListStudents() {
-    const arr = [];
-    const guillaume = {
-        id: 1, 
-        firstName: 'Guillaume', 
-        location: 'San Francisco'
-    };
-    
-    const james = {
-        id: 2,
-        firstName: 'James',
-        location: 'Columbia'
-    }
-
-    const serena = {
-        id: 5,
-        firstName: 'Serena',
-        location: 'San Francisco'
-    }
-
-    arr[0] = guillaume;
-    arr[1] = james;
-    arr[2] = serena;
-
-    return arr;
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
 }
