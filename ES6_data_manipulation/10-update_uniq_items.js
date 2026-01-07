64f2397da4734517721a2048d2ec7305bcf74739
Katherine Beringer <github@katbee.au>
2026-01-07 19:33:12 +1100

    Add error handling

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,8 @@
 export default function updateUniqueItems(groceriesList) {
+    if (!(groceriesList instanceof Map)) {
+        throw new Error("Cannot process");
+    }
+
     for (let [key, value] of groceriesList) {
         if (value === 1) {
             groceriesList.set(key, 100);
