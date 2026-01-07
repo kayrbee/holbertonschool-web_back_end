18ce3ae8bca9f2a4d37c251493a594a1ad9a8a93
Katherine Beringer <github@katbee.au>
2026-01-07 17:41:46 +1100

    Slightly better logic

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -4,13 +4,23 @@ export default function hasValuesFromArray(set, array) {
     let i = 0;
     let j = 0;
     let currentMatch = false;
-    while (i < set.size) {
+    while (i < array.length) {
         let currentSetValue = setIterator.next().value;
-        console.log("s: ", currentSetValue);
-        while (j < array.length) {
-            if (array[j] === currentSetValue) {
-                console.log("j: ", j, "value:", array[j]);
+        let currentArrayValue = array[i];
+        console.log("OUTER LOOP");
+        console.log("i: ", i, "set value: ", currentSetValue, "arr value: ", currentArrayValue);
+        while (j < set.size) {
+            console.log("INNER LOOP");
+            console.log("i: ", i, "set value: ", currentSetValue, "arr value: ", currentArrayValue);
+            if (currentArrayValue === currentSetValue) {
+                console.log("match");
                 currentMatch = true;
+                break;
+            }
+            else {
+                console.log("no match");
+                currentMatch = false;
+                break;
             }
             j++;
         }
