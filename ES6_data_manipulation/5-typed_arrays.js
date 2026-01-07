86a7faba87f26421c388d26eac8ea719fa50d036
Katherine Beringer <github@katbee.au>
2026-01-07 16:35:54 +1100

    Try wrapping the results?

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -5,8 +5,16 @@ export default function createInt8TypedArray(length, position, value) {
 
     const buffer = new ArrayBuffer(length);
     const int8arr = new Int8Array(buffer);
-    
+
     int8arr[position] = value;
 
-    return buffer;
+    let result = {
+        Dataview: {
+            byteLength: buffer.byteLength,
+            byteOffset: buffer.byteOffset,
+            buffer: buffer
+        }
+    }
+
+    return result;
 }
