3c755a916f49773942fbe12e2638b364fad88a32
Katherine Beringer <github@katbee.au>
2026-01-07 16:25:36 +1100

    Added error handling

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,12 @@
 export default function createInt8TypedArray(length, position, value) {
+    if (position < 0 || position >= length) {
+        throw new RangeError('Position outside range');
+    }
+
     const buffer = new ArrayBuffer(length);
     const int8arr = new Int8Array(buffer);
-    int8arr.fill(value, position, position + 1);
+    
+    int8arr[position] = value;
+
     return buffer;
 }
