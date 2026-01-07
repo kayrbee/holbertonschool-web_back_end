6e63d6663a29d2c6649b187895c748ce8571e316
Katherine Beringer <github@katbee.au>
2026-01-07 15:50:18 +1100

    Removed console.log statements

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -8,37 +8,28 @@ export default function updateStudentGradeByCity(studentList, city, newGrades) {
         }
 
         const students = studentList.filter(filterByLocation);
-        // console.log(students);
 
         function addGrades(students) {
             for (let element of newGrades) {
                 let { studentId, grade } = element;
-                // console.log(students.id);
                 if (students.id === studentId) {
-                    // console.log('Match!')
                     const studentGrade = {
                         ...students,
                         grade: grade
                     }
-                    // console.log(studentGrade);
                     return studentGrade;
                 }
-                // console.log('No match this time :(');
                 continue;
             }
             const studentGrade = {
                 ...students,
                 grade: 'N/A'
             }
-            // console.log(studentGrade);
             return studentGrade;
-            // console.log(element, studentId, grade);
         }
         const studentsWithGrade = students.map(addGrades);
-        // console.log(studentsWithGrade);
         return studentsWithGrade;
 
-        // let locationFilter = studentList.filter(filterByLocation);
     }
 
     return [];
