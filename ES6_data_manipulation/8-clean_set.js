7f5dba2fcabbb2239e331d2eaa26ea029de51936
Katherine Beringer <github@katbee.au>
2026-01-08 14:54:31 +1100

    Refactored handling missing args to meet checker

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,28 +1,30 @@
 export default function cleanSet(set, startString) {
+    let result = "";
     // Only run fn if set & startString are provided
-    if (startString !== undefined && set !== undefined) {
-        let result = "";
-        let i = 0;
-
-        if (startString.length === 0) {
-            return result;
-        }
-        
-        for (let item of set) {
-            // Don't include items which don't start with startString
-            if (!(item.startsWith(startString))) {
-                continue;
-            }
-            let startingIndex = startString.length;
-            let delim = "-";
-            // Print a delimiter between items
-            if (i != 0) {
-                result += delim;
-            }
-            result += item.substring(startingIndex, );
-            i++;
-        }
-        
+    if (startString === undefined || set === undefined) {
         return result;
-    } 
+    }
+
+    let i = 0;
+
+    if (startString.length === 0) {
+        return result;
+    }
+    
+    for (let item of set) {
+        // Don't include items which don't start with startString
+        if (!(item.startsWith(startString))) {
+            continue;
+        }
+        let startingIndex = startString.length;
+        let delim = "-";
+        // Print a delimiter between items
+        if (i != 0) {
+            result += delim;
+        }
+        result += item.substring(startingIndex, );
+        i++;
+    }
+        
+    return result; 
 }
