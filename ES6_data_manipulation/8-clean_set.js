236a9732c0ac43f15b4b936a71b2c7bc0765064f
Katherine Beringer <github@katbee.au>
2026-01-08 14:46:06 +1100

    Removed print statements

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,28 +1,21 @@
 export default function cleanSet(set, startString) {
     let result = "";
     let i = 0;
-    // console.log(set);
 
     if (startString.length === 0) {
         return result;
     }
 
     for (let item of set) {
-        // console.log(typeof item);
-        // console.log(item);
         if (!(item.startsWith(startString))) {
-            // console.log(item, " does not start with ", startString)
             continue;
         }
-        // console.log(item, "starts with ", startString);
         let startingIndex = startString.length;
         let delim = "-";
         if (i != 0) {
             result += delim;
         }
-        // console.log(startingIndex);
         result += item.substring(startingIndex, );
-        // console.log(result);
         i++;
     }
 
