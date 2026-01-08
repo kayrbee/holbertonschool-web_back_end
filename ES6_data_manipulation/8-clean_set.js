ab0f61c1fd07282d7905b6cf2cfc31f6d5ee5e5f
Katherine Beringer <github@katbee.au>
2026-01-08 14:52:03 +1100

    Handled undefined parameters

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,25 +1,28 @@
 export default function cleanSet(set, startString) {
-    let result = "";
-    let i = 0;
+    // Only run fn if set & startString are provided
+    if (startString !== undefined && set !== undefined) {
+        let result = "";
+        let i = 0;
 
-    if (startString.length === 0) {
+        if (startString.length === 0) {
+            return result;
+        }
+        
+        for (let item of set) {
+            // Don't include items which don't start with startString
+            if (!(item.startsWith(startString))) {
+                continue;
+            }
+            let startingIndex = startString.length;
+            let delim = "-";
+            // Print a delimiter between items
+            if (i != 0) {
+                result += delim;
+            }
+            result += item.substring(startingIndex, );
+            i++;
+        }
+        
         return result;
-    }
-
-    for (let item of set) {
-        // Don't include items which don't start with startString
-        if (!(item.startsWith(startString))) {
-            continue;
-        }
-        let startingIndex = startString.length;
-        let delim = "-";
-        // Print a delimiter between items
-        if (i != 0) {
-            result += delim;
-        }
-        result += item.substring(startingIndex, );
-        i++;
-    }
-
-    return result;
+    } 
 }
