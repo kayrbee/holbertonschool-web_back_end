3f90900aa66ea8073c5a9c49f7e07cd89856cb37
Katherine Beringer <github@katbee.au>
2026-01-09 17:46:16 +1100

    Implemented a promise.then() function:

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,12 @@
+export default function handleResponseFromAPI(promise) {
+    promise.then(function() {
+        console.log("Got a response from the API");
+        return {
+            status: 200,
+            body: 'success'
+        };
+    }, function() {
+        console.log("Got a response from the API");
+        return Error;
+    });
+}
