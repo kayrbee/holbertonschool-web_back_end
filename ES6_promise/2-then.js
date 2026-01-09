49b89962a7c06f4158e402e29dbbb99ded8de31c
Katherine Beringer <github@katbee.au>
2026-01-09 18:13:53 +1100

    Refactored and added promise return

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,12 +1,15 @@
 export default function handleResponseFromAPI(promise) {
-    promise.then(function() {
-        console.log("Got a response from the API");
+    return promise
+    .then(function() {
         return {
             status: 200,
             body: 'success'
         };
-    }, function() {
+    })
+    .catch(function() {
+        return new Error();
+    })
+    .finally(() => {
         console.log("Got a response from the API");
-        return Error;
     });
 }
