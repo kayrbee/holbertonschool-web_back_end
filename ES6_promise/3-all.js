d83ff6dfeb92191297cae70b1df7f013f49fe5d6
Katherine Beringer <github@katbee.au>
2026-01-12 10:58:53 +1100

    Used a promise return value | Created nested promises

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,19 @@
+import { uploadPhoto } from "./utils.js";
+import { createUser } from "./utils.js";
+
+export default function handleProfileSignup() {
+    return uploadPhoto()
+    .then(function(uploadPhotoResponse) {
+        const responseBody = uploadPhotoResponse.body;
+        return createUser()
+        .then(function(createUserResponse) {
+            console.log(responseBody, createUserResponse.firstName, createUserResponse.lastName);
+        })
+        .catch(function() {
+            return new Error();
+        })
+    })
+    .catch(function() {
+        return new Error();
+    });
+}
