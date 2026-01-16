007df690249331c0e33536e1f996111c93d98dcb
Katherine Beringer <github@katbee.au>
2026-01-16 15:23:19 +1100

    Refactored to use allSettled

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,27 +1,9 @@
 import signUpUser from "./4-user-promise";
 import uploadPhoto from "./5-photo-reject";
 
-export default async function handleProfileSignup(firstName, lastName, filename) {
-    const userPromise = signUpUser(firstName, lastName);
-    const photoPromise = uploadPhoto(filename);
-
-    return Promise.all([userPromise, photoPromise]
-    .map(p => p.catch(e => e)))
-    .then(function(values) {
-        let result = [];
-        for (let v of values) {
-            let r = {
-                status: 'fulfilled',
-                value: v
-            };
-            if (String(v).includes("Error")) {
-                r = {
-                    status: 'rejected',
-                    value: v.message
-                }
-            }
-            result.push(r);
-        }
-        return result;
-    })
+export default function handleProfileSignup(firstName, lastName, filename) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(filename),
+  ]);
 }
