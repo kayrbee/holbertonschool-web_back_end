4f58c938b8bc3539273db9161ebff6fe52bb8025
Katherine Beringer <github@katbee.au>
2026-01-16 17:25:53 +1100

    Refactored to handle EOF on child processes

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -5,13 +5,13 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  process.stdout.write(`Your name is: ${name}\r`);
-  rl.close();
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+rl.on('line', (name) => {
+    process.stdout.write(`Your name is: ${name}\r`);
 });
 
 // Handle application exit
 rl.on('close', () => {
-  console.log('This important software is now closing');
-  process.exit(0);
+  process.stdout.write('This important software is now closing\r');
 });
